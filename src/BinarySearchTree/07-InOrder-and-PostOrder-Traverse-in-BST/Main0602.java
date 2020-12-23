1e7ed7a9a8c6e69b6fb7f38bf3d3177935c0f477
lee <18710040761@163.com>
2020-12-23 16:59:32 +0800

    BST tudy

---
+++ src/BinarySearchTree/07-InOrder-and-PostOrder-Traverse-in-BST/Main0602.java
@@ -0,0 +1,28 @@
+package BinarySearchTree;
+
+public class Main0602 {
+
+    public static void main(String[] args) {
+
+        BinarySearchTree.BST02<Integer> bst = new BinarySearchTree.BST02<>();
+        int[] nums = {5, 3, 6, 8, 4, 2};
+        for(int num: nums)
+            bst.add(num);
+
+        /////////////////
+        //      5      //
+        //    /   \    //
+        //   3    6    //
+        //  / \    \   //
+        // 2  4     8  //
+        /////////////////
+        bst.preOrder();
+        System.out.println();
+
+        bst.inOrder();
+        System.out.println();
+
+        bst.postOrder();
+        System.out.println();
+    }
+}
